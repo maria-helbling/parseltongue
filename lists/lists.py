d3c1d72bf4bdc0955ddc9c232aff7a25c8a47a8c
mahelb <65257133+maria-helbling@users.noreply.github.com>
2020-10-08 21:30:05 -0700

    fun with lists

---
+++ lists/lists.py
@@ -0,0 +1,11 @@
+list1=[1,34,55,66666]
+list2=list()
+list2.append(list1[2])
+print(list2)
+# list2.append('mammu')
+# list2.append('Nummi')
+list2.append(100)
+print(list2)
+# can't sort a mixed list
+list2.sort()
+print(list2)
